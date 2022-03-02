9fb0656f307ae11867f7d8986a6439382c76fa59
44122196 <18588744489@163.com>
2022-03-02 11:53:46 +0800

    Update

--- app/src/main/java/com/wwe/taskdetail/TaskDetailFragment.kt
+++ app/src/main/java/com/wwe/taskdetail/TaskDetailFragment.kt
@@ -1,6 +1,86 @@
 package com.wwe.taskdetail
 
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import com.wwe.EventObserver
+import com.wwe.MyApplication
+import com.wwe.R
+import com.wwe.databinding.TaskdetailFragBinding
+import com.wwe.tasks.DELETE_RESULT_OK
+import com.wwe.util.setupRefreshLayout
+import com.wwe.util.setupSnackbar
 
 class TaskDetailFragment : Fragment() {
+    private lateinit var viewDataBinding: TaskdetailFragBinding
+
+    private val args: TaskDetailFragmentArgs by navArgs()
+
+    private val viewModel by viewModels<TaskDetailViewModel> {
+        TaskDetailViewModelFactory((requireContext().applicationContext as MyApplication).taskRepository)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupFab()
+        view.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
+        setupNavigation()
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+    }
+
+    private fun setupNavigation() {
+        viewModel.deleteTaskEvent.observe(viewLifecycleOwner, EventObserver {
+            val action = TaskDetailFragmentDirections
+                .actionTaskDetailFragmentToTasksFragment(DELETE_RESULT_OK)
+            findNavController().navigate(action)
+        })
+        viewModel.editTaskEvent.observe(viewLifecycleOwner, EventObserver {
+            val action = TaskDetailFragmentDirections
+                .actionTaskDetailFragmentToAddEditTaskFragment(
+                    args.taskId,
+                    resources.getString(R.string.edit_task)
+                )
+            findNavController().navigate(action)
+        })
+    }
+
+    private fun setupFab() {
+        activity?.findViewById<View>(R.id.edit_task_fab)?.setOnClickListener {
+            viewModel.editTask()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.taskdetail_frag, container, false)
+        viewDataBinding = TaskdetailFragBinding.bind(view).apply {
+            viewmodel = viewModel
+        }
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+
+        viewModel.start(args.taskId)
+
+        setHasOptionsMenu(true)
+        return view
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_delete -> {
+                viewModel.deleteTask()
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.taskdetail_fragment_menu, menu)
+    }
 }
